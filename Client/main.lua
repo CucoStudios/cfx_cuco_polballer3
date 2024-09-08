894d4cfa6ebf86e1713fd3f92485643fc4de02d5
FilipeCuco <filipecuco0@gmail.com>
2024-09-08 01:25:19 +0100

    Initial commit

---
+++ Client/main.lua
@@ -0,0 +1,7 @@
+CreateThread(function()
+    -- LIVERY POLICE --
+    AddTextEntry("PB3_LP", "Livery Police")
+    AddTextEntry("PB3_LPK9", "Livery Police K9")
+    AddTextEntry("PB3_LS", "Livery Sheriff")
+    AddTextEntry("PB3_LSK9", "Livery Sheriff K9")
+end)
